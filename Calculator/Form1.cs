bd1d3e76a2fb14f151356bc6044bb6aa7c9eb23f
wkisielewicz <wkisielewicz@criticue.com>
2017-04-01 12:33:11 +0200

    Add calculator buttons.

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -16,5 +16,15 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
